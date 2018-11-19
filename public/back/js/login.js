53988e5a9af11308738f199198b855745ed51a9b
pyb123 <1196585756@qq.com>
2018-11-19 14:57:14 +0800

    重置按钮功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -2,12 +2,11 @@
 
 $(function() {
   $("#form").bootstrapValidator({
-
     // 配置小图标
     feedbackIcons: {
-      valid: "glyphicon glyphicon-ok",  //校验成功
-      invalid: "glyphicon glyphicon-remove",  //校验失败
-      validating: "glyphicon glyphicon-refresh"  //校验中
+      valid: "glyphicon glyphicon-ok", //校验成功
+      invalid: "glyphicon glyphicon-remove", //校验失败
+      validating: "glyphicon glyphicon-refresh" //校验中
     },
 
     // 配置校验字段(配置之前，需要先配置input的name)
@@ -46,45 +45,40 @@ $(function() {
     }
   });
 
+  //   2-注册表单校验成功事件，在事件中阻止默认成功的表单提交，通过ajax提交
 
-
-
-//   2-注册表单校验成功事件，在事件中阻止默认成功的表单提交，通过ajax提交
-
-  $('#form').on('success.form.bv',function(e){
+  $("#form").on("success.form.bv", function(e) {
     //   阻止默认的表单提交
-      e.preventDefault();
+    e.preventDefault();
 
-      $.ajax({
-          type: 'post',
-          url: '/employee/employeeLogin',
-          data: $('#form').serialize(),
-          dataType: 'json',
-          success: function(info){
-            //   console.log(info);
-            if(info.success){
-                // 
-                location.href = 'index.html';
-            }
-            if(info.error === 1000){
-                alert(info.message);
-            }
-            if(info.error === 1001){
-                alert(info.message);
-            }
-          }
-      })
-  })
+    $.ajax({
+      type: "post",
+      url: "/employee/employeeLogin",
+      data: $("#form").serialize(),
+      dataType: "json",
+      success: function(info) {
+        //   console.log(info);
+        if (info.success) {
+          //
+          location.href = "index.html";
+        }
+        if (info.error === 1000) {
+          alert(info.message);
+        }
+        if (info.error === 1001) {
+          alert(info.message);
+        }
+      }
+    });
+  });
 
-
-
-
-
-//   3-重置功能：本身reset按钮，可以重置内容，需要调用表单校验插件的方法
-  $('[type="reset"]').click(function(){
+  //   3-重置功能：本身reset按钮，可以重置内容，需要调用表单校验插件的方法
+  $('[type="reset"]').click(function() {
     // resetForm
-    // resetFrom 如果传true，表示内容和状态都重置
+    // resetForm 如果传true，表示内容和状态都重置
     // 如果不传参，只重置状态
-      $('#form').data('bootstrapValidator').resetFrom();
-  })
+    $("#form")
+      .data("bootstrapValidator")
+      .resetForm();
+  });
 });
