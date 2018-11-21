c45b481163d5877fe0bb1a6ef7957ea6ae64eddb
pyb123 <1196585756@qq.com>
2018-11-21 08:46:03 +0800

    登录页面完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -25,6 +25,9 @@ $(function() {
             min: 2,
             max: 6,
             message: "用户名长度必须是2-6位"
+          },
+          callback:{
+            message: '用户名不存在'
           }
         }
       },
@@ -39,6 +42,9 @@ $(function() {
             min: 6,
             max: 12,
             message: "密码长度必须是6-12位"
+          },
+          callback:{
+            message: '密码错误'
           }
         }
       }
@@ -64,9 +70,12 @@ $(function() {
         }
         if (info.error === 1000) {
           alert(info.message);
+          // 用户名不存在
+          // updateStatus('字段名称'，)
+          $('#form').data('bootstrapValidator').updateStatus('username','INVALID','callback');
         }
         if (info.error === 1001) {
-          alert(info.message);
+          $('#form').data('bootstrapValidator').updateStatus('password','INVALID','callback');
         }
       }
     });
