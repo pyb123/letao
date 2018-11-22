6a12087aa0c1c3853c131faec9fda5ccd372cec4
pyb123 <1196585756@qq.com>
2018-11-22 10:49:28 +0800

    一级分类添加完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -67,4 +67,31 @@ $(function() {
       }
     }
   });
+
+  // 4-注册表单校验成功事件，阻止默认的表单提交，通过ajax提交
+  $('#form').on('success.form.bv',function(e){
+    // 阻止默认提交
+      e.preventDefault();
+
+      //ajax请求
+      $.ajax({
+          type:'post',
+          url: '/category/addTopCategory',
+          data: $('#form').serialize(),
+          dataType:'json',
+          success: function(info){
+            console.log(info);
+            if(info.success){
+                // 添加成功
+                // 关闭模态框，
+                $('#addModal').modal('hide');
+                // 重新渲染第一页
+                currentPage = 1;
+                render();
+                // 内容和状态需要充值
+                $('#form').data('bootstrapValidator').resetForm(true);
+            }
+          }
+      })
+  })
 });
